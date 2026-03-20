b6ed2fd260fb5833c9f72ab0e20901bf5e51e16e
letuyethan <hantuyetle20@gmail.com>
2026-03-20 22:07:53 +0000

    Deploying to gh-pages from @ letuyethan/letuyethan.github.io@d00f778f7fcfa75e5fe093c6d92cede2551f373e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -474,7 +474,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv/CV_en.pdf", "_blank");
         },
       },{
         id: 'social-email',
